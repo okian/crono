4efc5cd2b5b1e67a0e70698ab87753d309fab877
Kian Ostad <c@kianostad.com>
2019-03-09 16:04:02 +0330

    limit worker

--- main.go
+++ main.go
@@ -218,65 +218,68 @@ func checkWiki(u *url.URL) bool {
 }
 
 func worker(ctx context.Context, urls chan *url.URL) {
-	c := colly.NewCollector(func(x *colly.Collector) {
-		x.AllowedDomains = []string{
-			"gadgetnews.net",
-			"www.irna.ir",
-			"www.farsnews.com",
-			"dictionary.abadis.ir",
-			"www.khabaronline.ir",
-			"www.varzesh3.com",
-			"www.aparat.com",
-			"www.entekhab.ir",
-			"www.bbc.com",
-			"fa.wikipedia.org",
-			"fa.wiktionary.org",
-			"www.vajehyab.com",
-			"www.hidoctor.ir",
-			"tebna.ir",
-			"www.sid.ir",
-			"digiato.com",
-			"zoomlife.ir",
-			"www.zoomit.ir",
-			"www.chetor.com",
-			"www.ghesticlub.com",
-			"www.radiofarda.com",
-		}
-		x.URLFilters = []*regexp.Regexp{
-			ptmaker("gadgetnews.net"),
-			ptmaker("www.irna.ir"),
-			ptmaker("www.isna.ir"),
-			ptmaker("www.ilna.ir"),
-			ptmaker("www.digikala.com"),
-			ptmaker("www.farsnews.com"),
-			ptmaker("dictionary.abadis.ir"),
-			ptmaker("www.khabaronline.ir"),
-			ptmaker("www.varzesh3.com"),
-			ptmaker("www.aparat.com"),
-			ptmaker("www.entekhab.ir"),
-			ptmaker("www.bbc.com/persian/"),
-			ptmaker("fa.wikipedia.org/wiki/"),
-			ptmaker("fa.wiktionary.org/wiki/"),
-			ptmaker("www.vajehyab.com/"),
-			ptmaker("www.hidoctor.ir"),
-			ptmaker("tebna.ir"),
-			ptmaker("www.sid.ir"),
-			ptmaker("digiato.com"),
-			ptmaker("zoomlife.ir"),
-			ptmaker("www.zoomit.ir"),
-			ptmaker("www.chetor.com"),
-			ptmaker("www.ghesticlub.com"),
-			ptmaker("www.radiofarda.com"),
-		}
-		x.AllowURLRevisit = false
+	c := colly.NewCollector(
+		colly.Async(true),
+		func(x *colly.Collector) {
+			x.AllowedDomains = []string{
+				"gadgetnews.net",
+				"www.irna.ir",
+				"www.farsnews.com",
+				"dictionary.abadis.ir",
+				"www.khabaronline.ir",
+				"www.varzesh3.com",
+				"www.aparat.com",
+				"www.entekhab.ir",
+				"www.bbc.com",
+				"fa.wikipedia.org",
+				"fa.wiktionary.org",
+				"www.vajehyab.com",
+				"www.hidoctor.ir",
+				"tebna.ir",
+				"www.sid.ir",
+				"digiato.com",
+				"zoomlife.ir",
+				"www.zoomit.ir",
+				"www.chetor.com",
+				"www.ghesticlub.com",
+				"www.radiofarda.com",
+			}
+			x.URLFilters = []*regexp.Regexp{
+				ptmaker("gadgetnews.net"),
+				ptmaker("www.irna.ir"),
+				ptmaker("www.isna.ir"),
+				ptmaker("www.ilna.ir"),
+				ptmaker("www.digikala.com"),
+				ptmaker("www.farsnews.com"),
+				ptmaker("dictionary.abadis.ir"),
+				ptmaker("www.khabaronline.ir"),
+				ptmaker("www.varzesh3.com"),
+				ptmaker("www.aparat.com"),
+				ptmaker("www.entekhab.ir"),
+				ptmaker("www.bbc.com/persian/"),
+				ptmaker("fa.wikipedia.org/wiki/"),
+				ptmaker("fa.wiktionary.org/wiki/"),
+				ptmaker("www.vajehyab.com/"),
+				ptmaker("www.hidoctor.ir"),
+				ptmaker("tebna.ir"),
+				ptmaker("www.sid.ir"),
+				ptmaker("digiato.com"),
+				ptmaker("zoomlife.ir"),
+				ptmaker("www.zoomit.ir"),
+				ptmaker("www.chetor.com"),
+				ptmaker("www.ghesticlub.com"),
+				ptmaker("www.radiofarda.com"),
+			}
+			x.AllowURLRevisit = false
 
-	})
+		})
 	c.WithTransport(&http.Transport{
 		DialContext: (&net.Dialer{
 			Timeout:   10 * time.Second,
 			KeepAlive: 10 * time.Second,
 			DualStack: true,
 		}).DialContext,
+		DisableKeepAlives: true,
 		MaxIdleConns:          20,
 		IdleConnTimeout:       90 * time.Second,
 		TLSHandshakeTimeout:   10 * time.Second,
@@ -286,7 +289,7 @@ func worker(ctx context.Context, urls chan *url.URL) {
 		DomainGlob:  "*",
 		Delay:       time.Millisecond * 150,
 		RandomDelay: time.Second * 2,
-		Parallelism: 3,
+		Parallelism: 10,
 	})
 	if err != nil {
 		log.Fatal(err)
